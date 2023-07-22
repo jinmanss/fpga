faee222761e905bf4ec661f45337bd21d13d1192
jinmanss <65714822+jinmanss@users.noreply.github.com>
2023-07-22 18:22:53 +0900

    Update carry_ripple_adder.sv

--- carry_ripple_adder.sv
+++ carry_ripple_adder.sv
@@ -1,23 +1,23 @@
-module full_adder (
-	input a, b, cin,
-	output s, cout
+module full_adder (			             // 전가산기 모듈 생성
+	input a, b, cin,                             // 인풋으로 a,b,carry in 을 선언
+	output s, cout				     // 아웃풋으로 sum, carrry out을 선
 );
-	assign s = a ^ b ^ cin;
-	assign cout = (a & b) | (cin & (a ^ b));
-endmodule
+	assign s = a ^ b ^ cin;                      // assign을 통해서 
+	assign cout = (a & b) | (cin & (a ^ b));    
+endmodule                                            // 전가산기 모듈 끝내는 구문
+ 
 
 
 
-
-module carry_ripple_adder (
-	input	[7:0] a, b, 
-	input   cin,
+module carry_ripple_adder (                         // 캐리리플가산기 모듈 생성
+	input	[7:0] a, b,                          // 인풋으로 8비트의 a,b와 carry in 을 선언, 아웃풋으로 8비트의 sum과 carry out을 선언
+	input   cin, 
 	output 	[7:0] s, 
 	output  cout
 );
 
 
-generate
+generate                                           // 반복문을 생성하는 구
 	wire [8:0] carry;
 
 	assign carry[0] = cin;
